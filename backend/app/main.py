4b1b0ffc24256f3b7bb9e1f486d86e8e91209105
sanskrutipasalkar2002 <sanskrutipasalkar10@gmail.com>
2026-03-09 15:54:44 +0530

    Change backend main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "https://kpcl-warranty-claims-frontend.onrender.com",
+        "https://kpclupdated-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
